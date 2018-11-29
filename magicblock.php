58c5e35ca1f34a418b228eedc2b8f2569f7f33b9
Will Delphia <willdelphia@gmail.com>
2018-11-28 20:49:25 -0600

    Initial commit

---
+++ magicblock.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name:  Magic Block
+Description:  Registers a container block with the Gutenberg (WP5.0) editor. This block has settings for element type (div, section, etc), id, classname, and style (inline css). It is designed for people who want full control over post HTML, or wish to use the editor to create complex layouts (Flexbox, CSS Grid, Bootstrap, etc) 
+Version:      1.0
+Author:       Will Delphia
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  magic-block
+*/
+
+function magicblock_register_scripts() {
+
+    wp_register_script(
+        'magicblock-main-script',
+        plugins_url( 'js/magicblock.js', __FILE__ ),
+        array( 'wp-blocks', 'wp-element' )
+    );
+
+    wp_register_style(
+        'magicblock-css',
+        plugins_url( 'css/magicblock.css', __FILE__ ),
+        array(),
+        filemtime( plugin_dir_path( __FILE__ ) . 'css/magicblock.css' )
+    );
+
+    register_block_type( 'magicblock/magicblock', array(
+        'editor_script' => 'magicblock-main-script',
+        'editor_style'  => 'wd-magicblock-css'
+        )
+    );
+}
+add_action( 'init', 'magicblock_register_scripts' );
---
+++ plugin-readme.txt
@@ -0,0 +1,12 @@
+=== Magic Block ===
+Contributors: willdelphia
+Tags: gutenberg, block, container, html element, flexbox, css grid, tag
+Requires at least: 4.9.8
+Tested up to: 5.0
+License: GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+
+Registers a container block with the Gutenberg (WP5.0) editor. This block has settings for element type (div, section, etc), id, classname, and style (inline css). It is designed for people who want full control over post HTML, or wish to use the editor to create complex layouts (Flexbox, CSS Grid, Bootstrap, etc) 
+
+== Description ==
+Registers a container block with the Gutenberg (WP5.0) editor. This block has settings for element type (div, section, etc), id, classname, and style (inline css). 
